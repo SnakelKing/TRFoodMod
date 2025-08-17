b9a2d1d7a44d75fcf15b289dc2738f1ed4b5ced3
SnakelKing <crymaryanhesap53@gmail.com>
2025-08-17 21:05:01 +0300

    Greenbean, Vanilla, Turmeric, Rice, Pepper, Peanut, Oat, Mustard, Ginger, Basil, Barley, Zucchini, Yam, Turnip, Tomatillo, Green Onion,Sweet Potato, Spinach, Soybean, Rutabaga, Rhubarb, Radish, Onion, Lettuce, Squash, Chile Pepper, Hops, Garlic, Kale, Cucumber,

---
+++ src/main/java/com/cefu/trfoodmod/block/HopsCropBlock.java
@@ -0,0 +1,36 @@
+package com.cefu.trfoodmod.block;
+
+import com.cefu.trfoodmod.core.init.ItemInit;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+
+
+public class HopsCropBlock extends CropBlock {
+    public static final int MAX_AGE = 3;
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_3;
+
+    public HopsCropBlock(Properties pProperties) { super(pProperties); }
+
+    @Override
+    protected ItemLike getBaseSeedId() { return ItemInit.HOPS_SEEDS.get(); }
+
+    @Override
+    public IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(AGE);
+    }
+}
